0647d22275df8bddde56eaa736748a9e3f137744
exy <lynachorf@gmail.com>
2025-01-01 01:01:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// Script pour permettre de naviguer sans défilement
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
